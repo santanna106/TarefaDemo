b2b8758cb52cc17e6aeeedb7faa6ee14ef1e5dad
Gabriel Andrade <gabriel.santana@gmail.com>
2022-03-26 07:20:51 -0300

    First Minimal Api .net core 6.0

---
+++ Program.cs
@@ -0,0 +1,73 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("TarefasDb"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/tarefas", async (AppDbContext db) => { return await db.Tarefas.ToListAsync();
+});
+
+app.MapGet("/tarefas/{id}",async(int id, AppDbContext db) => 
+    await db.Tarefas.FindAsync(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound());
+
+app.MapGet("/tarefas/concluida",async (AppDbContext db) => 
+    await db.Tarefas.Where(t => t.IsConcluida).ToListAsync());
+
+
+app.MapDelete("/tarefas/{id}",async (int id,AppDbContext db) =>{
+    var tarefa = await db.Tarefas.FindAsync(id);
+
+    if(tarefa is null) return Results.NotFound();
+    
+    db.Tarefas.Remove(tarefa);
+    await db.SaveChangesAsync();
+    return Results.Ok(tarefa);
+
+});
+
+app.MapPut("/tarefas/{id}",async (int id,Tarefa inputTarefa,AppDbContext db) => {
+    var tarefa = await db.Tarefas.FindAsync(id);
+    if(tarefa is null) return Results.NotFound();
+
+    tarefa.Nome = inputTarefa.Nome;
+    tarefa.IsConcluida = inputTarefa.IsConcluida;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapPost("/tarefas",async (Tarefa tarefa,AppDbContext db) => {
+    db.Tarefas.Add(tarefa);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tarefas/{tarefa.Id}",tarefa);
+});
+
+app.Run();
+
+class Tarefa{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public bool IsConcluida { get; set; }
+}
+
+class AppDbContext : DbContext{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+    {    }
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+}
